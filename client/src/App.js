223feef83c287ac9ea69b010bd75b08fa725cdb3
joe@internpursuit.tech <joe@internpursuit.tech>
2022-08-28 16:45:46 -0400

    added react app and started on UI for social app

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <MenuBar />
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
